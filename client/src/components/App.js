42b726bf24ede68ae2fc323535c570315e512390
Nadine Ju <nadine.jura@online.de>
2020-03-02 15:08:16 -0800

    routes and basic components created

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 
 // npm react-router-dom imports
-import { BrowserRouter, Route } from 'react-router-dom';
+// all <a></a> tags should be replaced with <Link />
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-    return <div>PageOne</div>
-};
-
-const PageTwo = () => {
-    return <div>PageTwo</div>
-};
+// import components
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path='/' exact component={PageOne} />
-                    <Route path='/pagetwo' exact component={PageTwo} />
+                    <Route path='/' exact component={StreamList} />
+                    <Route path='/streams/new' exact component={StreamCreate} />
+                    <Route path='/streams/edit' exact component={StreamEdit} />
+                    <Route path='/streams/delete' exact component={StreamDelete} />
+                    <Route path='/streams/show' exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
