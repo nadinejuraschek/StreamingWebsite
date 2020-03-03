6126c5df0b2d896782ed1ab06d0b5a5fd3e3bbd0
Nadine Ju <nadine.jura@online.de>
2020-03-02 23:47:24 -0800

    Google authentication functions added

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
-    componentDidMount = () => {
+    state = { isSignedIn: null };
+
+    componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientID: '52296544726-boi1el5ahubnub3fftjteh95f900300i.apps.googleusercontent.com',
+                clientId: '52296544726-boi1el5ahubnub3fftjteh95f900300i.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     };
 
-      render() {
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    renderAuthBtn() {
+        if (this.state.isSignedIn === null) {
+            return (
+                <div>I don't know if I am signed in!</div>
+            );
+        } else if (this.state.isSignedIn) {
+            return (
+                <div>I am signed in!</div>
+            );
+        } else {
+            return (
+                <div>I am not signed in.</div>
+            );
+        }
+    };
+
+    render() {
         return (
-            <div className='Google Auth' />
+            <div>{this.renderAuthBtn()}</div>
         );
     };
 };
